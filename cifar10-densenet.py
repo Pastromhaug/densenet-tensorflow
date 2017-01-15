9d056fd3601a6f3403eb35cd30d1a0aba577eade
Per Andre Stromhaug <pas282@cornell.edu>
2017-01-14 22:03:19 -0500

    got to run on my machine

--- cifar10-densenet.py
+++ cifar10-densenet.py
@@ -5,9 +5,9 @@ import numpy as np
 import tensorflow as tf
 import argparse
 import os
+import tensorpack as tp
 
-
-from tensorpack import *
+# from tensorpack import *
 from tensorpack.tfutils.symbolic_functions import *
 from tensorpack.tfutils.summary import *
 
@@ -17,20 +17,20 @@ Code is developed based on Yuxin Wu's ResNet implementation: https://github.com/
 Results using DenseNet (L=40, K=12) on Cifar10 with data augmentation: ~5.77% test error.
 
 Running time:
-On one TITAN X GPU (CUDA 7.5 and cudnn 5.1), the code should run ~5iters/s on a batch size 64.  
+On one TITAN X GPU (CUDA 7.5 and cudnn 5.1), the code should run ~5iters/s on a batch size 64.
 """
 
 BATCH_SIZE = 64
 
-class Model(ModelDesc):
+class Model(tp.ModelDesc):
     def __init__(self, depth):
         super(Model, self).__init__()
         self.N = int((depth - 4)  / 3)
-        self.growthRate =12 
+        self.growthRate =12
 
     def _get_input_vars(self):
-        return [InputVar(tf.float32, [None, 32, 32, 3], 'input'),
-                InputVar(tf.int32, [None], 'label')
+        return [tp.InputVar(tf.float32, [None, 32, 32, 3], 'input'),
+                tp.InputVar(tf.int32, [None], 'label')
                ]
 
     def _build_graph(self, input_vars):
@@ -38,14 +38,14 @@ class Model(ModelDesc):
         image = image / 128.0 - 1
 
         def conv(name, l, channel, stride):
-            return Conv2D(name, l, channel, 3, stride=stride,
+            return tp.Conv2D(name, l, channel, 3, stride=stride,
                           nl=tf.identity, use_bias=False,
                           W_init=tf.random_normal_initializer(stddev=np.sqrt(2.0/9/channel)))
         def add_layer(name, l):
             shape = l.get_shape().as_list()
             in_channel = shape[3]
             with tf.variable_scope(name) as scope:
-                c = BatchNorm('bn1', l)
+                c = tp.BatchNorm('bn1', l)
                 c = tf.nn.relu(c)
                 c = conv('conv1', c, self.growthRate, 1)
                 l = tf.concat(3, [c, l])
@@ -55,10 +55,10 @@ class Model(ModelDesc):
             shape = l.get_shape().as_list()
             in_channel = shape[3]
             with tf.variable_scope(name) as scope:
-                l = BatchNorm('bn1', l)
+                l = tp.BatchNorm('bn1', l)
                 l = tf.nn.relu(l)
-                l = Conv2D('conv1', l, in_channel, 1, stride=1, use_bias=False, nl=tf.nn.relu)
-                l = AvgPooling('pool', l, 2)
+                l = tp.Conv2D('conv1', l, in_channel, 1, stride=1, use_bias=False, nl=tf.nn.relu)
+                l = tp.AvgPooling('pool', l, 2)
             return l
 
 
@@ -69,7 +69,7 @@ class Model(ModelDesc):
                 for i in range(self.N):
                     l = add_layer('dense_layer.{}'.format(i), l)
                 l = add_transition('transition1', l)
-            
+
             with tf.variable_scope('block2') as scope:
 
                 for i in range(self.N):
@@ -80,15 +80,15 @@ class Model(ModelDesc):
 
                 for i in range(self.N):
                     l = add_layer('dense_layer.{}'.format(i), l)
-            l = BatchNorm('bnlast', l)
+            l = tp.BatchNorm('bnlast', l)
             l = tf.nn.relu(l)
-            l = GlobalAvgPooling('gap', l)
-            logits = FullyConnected('linear', l, out_dim=10, nl=tf.identity)
+            l = tp.GlobalAvgPooling('gap', l)
+            logits = tp.FullyConnected('linear', l, out_dim=10, nl=tf.identity)
 
             return logits
 
         logits = dense_net("dense_net")
-        
+
         prob = tf.nn.softmax(logits, name='output')
 
         cost = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, label)
@@ -99,7 +99,7 @@ class Model(ModelDesc):
         add_moving_summary(tf.reduce_mean(wrong, name='train_error'))
 
         # weight decay on all W
-        wd_cost = tf.mul(1e-4, regularize_cost('.*/W', tf.nn.l2_loss), name='wd_cost')
+        wd_cost = tf.mul(1e-4, tp.regularize_cost('.*/W', tf.nn.l2_loss), name='wd_cost')
         add_moving_summary(cost, wd_cost)
 
         add_param_summary([('.*/W', ['histogram'])])   # monitor W
@@ -108,51 +108,51 @@ class Model(ModelDesc):
 
 def get_data(train_or_test):
     isTrain = train_or_test == 'train'
-    ds = dataset.Cifar10(train_or_test)
+    ds = tp.dataset.Cifar10(train_or_test)
     pp_mean = ds.get_per_pixel_mean()
     if isTrain:
         augmentors = [
-            imgaug.CenterPaste((40, 40)),
-            imgaug.RandomCrop((32, 32)),
-            imgaug.Flip(horiz=True),
-            #imgaug.Brightness(20),
-            #imgaug.Contrast((0.6,1.4)),
-            imgaug.MapImage(lambda x: x - pp_mean),
+            tp.imgaug.CenterPaste((40, 40)),
+            tp.imgaug.RandomCrop((32, 32)),
+            tp.imgaug.Flip(horiz=True),
+            #tp.imgaug.Brightness(20),
+            #tp.imgaug.Contrast((0.6,1.4)),
+            tp.imgaug.MapImage(lambda x: x - pp_mean),
         ]
     else:
         augmentors = [
-            imgaug.MapImage(lambda x: x - pp_mean)
+            tp.imgaug.MapImage(lambda x: x - pp_mean)
         ]
-    ds = AugmentImageComponent(ds, augmentors)
-    ds = BatchData(ds, BATCH_SIZE, remainder=not isTrain)
+    ds = tp.AugmentImageComponent(ds, augmentors)
+    ds = tp.BatchData(ds, BATCH_SIZE, remainder=not isTrain)
     if isTrain:
-        ds = PrefetchData(ds, 3, 2)
+        ds = tp.PrefetchData(ds, 3, 2)
     return ds
 
 def get_config():
     log_dir = 'train_log/cifar10-single-fisrt%s-second%s-max%s' % (str(args.drop_1), str(args.drop_2), str(args.max_epoch))
-    logger.set_logger_dir(log_dir, action='n')
+    tp.logger.set_logger_dir(log_dir, action='n')
 
     # prepare dataset
     dataset_train = get_data('train')
     step_per_epoch = dataset_train.size()
     dataset_test = get_data('test')
 
-    sess_config = get_default_sess_config(0.9)
+    sess_config = tp.get_default_sess_config(0.9)
 
-    get_global_step_var()
+    tp.get_global_step_var()
     lr = tf.Variable(0.1, trainable=False, name='learning_rate')
-    tf.scalar_summary('learning_rate', lr)
+    tf.summary.scalar('learning_rate', lr)
 
-    return TrainConfig(
-        dataset=dataset_train,
+    return tp.TrainConfig(
+        dataflow=dataset_train,
         optimizer=tf.train.MomentumOptimizer(lr, 0.9, use_nesterov=True),
-        callbacks=Callbacks([
-            StatPrinter(),
-            ModelSaver(),
-            InferenceRunner(dataset_test,
-                [ScalarStats('cost'), ClassificationError()]),
-            ScheduledHyperParamSetter('learning_rate',
+        callbacks=tp.Callbacks([
+            tp.StatPrinter(),
+            tp.ModelSaver(),
+            tp.InferenceRunner(dataset_test,
+                [tp.ScalarStats('cost'), tp.ClassificationError()]),
+            tp.ScheduledHyperParamSetter('learning_rate',
                                       [(1, 0.1), (args.drop_1, 0.01), (args.drop_2, 0.001)])
         ]),
         session_config=sess_config,
@@ -179,4 +179,5 @@ if __name__ == '__main__':
         config.session_init = SaverRestore(args.load)
     if args.gpu:
         config.nr_tower = len(args.gpu.split(','))
-    SyncMultiGPUTrainer(config).train()
+    # tp.SyncMultiGPUTrainer(config).train()
+    tp.SimpleTrainer(config).train()
